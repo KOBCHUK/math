566528902d70c181f7ee502d62060d5958e0ffdd
Eugen Kobchuk <irravadi@gmail.com>
2023-03-19 11:37:46 +0200

    hm ex_5 task done

--- math.py
+++ math.py
@@ -1,21 +1,11 @@
 
-num1 = int(input('num = '))
-
-print(num1[::-1])
-
-#digit = num1 % 10
-#num1 = num1 // 10
-#num2 = num2 * 10
-#num2 = num2 + digit
-#print(f'reciplocal number = {num2}')
-
-
-#n1 = input('num=' )
-#n_list = list(n1)
-#n_list.reverse()
-#n2 = "".join(n_list)
-#print(f'reciplocal number = {n2}')
-
+num = int(input('num = ')) 
+number_1 = int(num/1000)
+number_2 = int(num/100)%10
+number_3 = int(num/10)%10
+number_4 = int(num%10)
+result_number = (number_4*1000)+(number_3*100)+(number_2*10)+number_1
+print(f'result = {result_number}')
 
 
 
