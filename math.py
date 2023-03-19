6a9d0ad60e1fb54662befdd1b728b73e80e0a240
Eugen Kobchuk <irravadi@gmail.com>
2023-03-19 11:29:45 +0200

    hm ex_5 task done

--- math.py
+++ math.py
@@ -1,1 +1,22 @@
 
+num1 = int(input('num = '))
+
+print(num1[::-1])
+
+#digit = num1 % 10
+#num1 = num1 // 10
+#num2 = num2 * 10
+#num2 = num2 + digit
+#print(f'reciplocal number = {num2}')
+
+
+#n1 = input('num=' )
+#n_list = list(n1)
+#n_list.reverse()
+#n2 = "".join(n_list)
+#print(f'reciplocal number = {n2}')
+
+
+
+
+
