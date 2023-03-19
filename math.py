dc3d17cdd037f77ef556e3dc7a5b8efee588f1ff
Eugen Kobchuk <irravadi@gmail.com>
2023-03-19 10:39:42 +0200

    hm ex_1 task done

--- math.py
+++ math.py
@@ -1,1 +1,9 @@
 
+num_1 = int(input('num_1 = '))
+num_2 = int(input('num_2 = '))
+num_3 = int(input('num_3 = '))
+res_1 = num_1*100
+res_2 = num_2*10
+res_3 = num_3*1
+res = res_1 + res_2 + res_3
+print(res)
