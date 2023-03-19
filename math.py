1031638e9e18deace71b00935b40619263b1cd58
Eugen Kobchuk <irravadi@gmail.com>
2023-03-19 10:59:05 +0200

    hm ex_3 task done

--- math.py
+++ math.py
@@ -1,1 +1,11 @@
 
+metres = float(input('metres = '))
+centimetres = metres*100
+decimeteres = metres*10
+milimetres = metres*1000
+mile = metres*0.00621731
+
+print(f'centimetres = {centimetres}')
+print(f'decimeteres = {decimeteres}')
+print(f'milimetres = {milimetres}')
+print(f'mile = {mile}')
