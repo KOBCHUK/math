761fc268fe47695c72ba8a6aa7e7e9394a4185ef
Eugen Kobchuk <irravadi@gmail.com>
2023-03-19 10:46:50 +0200

    hm ex_2 task done

--- math.py
+++ math.py
@@ -1,4 +1,9 @@
 
-number = int(input('number ='))
-num_1 = number%1000
-print(num_1)
+number = int(input('number = '))
+num_1 = int(number/1000)
+num_2 = int((number/100)%10)
+num_3 = int((number/10)%10)
+num_4 = int(number%10)
+mult = num_1*num_2*num_3*num_4
+print(mult)
+
